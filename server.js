671ca5e6969ccf2c4e7eb1d6baa4163c18638df7
Alexander Turner <alexander_k_turner@homedepot.com>
2020-07-11 16:49:45 -0400

    Modify server.js to modify cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/build')); //__dir and not _dir
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+// app.use(res, express.static(__dirname + '/build')); //__dir and not _dir
 const port = process.env.PORT || 8080; // you can use any port
 app.listen(port);
 console.log('server on: ' + port);
