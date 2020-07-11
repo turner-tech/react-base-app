fa574e46411a1e06b24115a6725788f0d27d4f82
Alexander Turner <alexander_k_turner@homedepot.com>
2020-07-11 03:10:37 -0400

    Include env files

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/build')); //__dir and not _dir
-const port = 8080; // you can use any port
+const port = process.env.PORT || 8080; // you can use any port
 app.listen(port);
 console.log('server on: ' + port);
